3d79c7b24927f6b46320443f273f7ea709b2a612
Louis Tsang <kawingt2@gmail.com>
2025-03-04 23:39:31 +0800

    rearranged the code and add comment

--- ascii.py
+++ ascii.py
@@ -1,27 +1,9 @@
 from PIL import Image
 import math
 
-image = Image.open("test2.png")
-image = image.convert("RGB")
-width, height = image.width, image.height
-
-print("Successfully loaded image!")
-print(f"Image size: {width} x {height}")
-
+ASCII_CHAR_SET = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@" # 91 Chars
 MAX_HEIGHT = 80
 MAX_WIDTH = 60
-ratio = min(MAX_WIDTH/width, MAX_HEIGHT/height)
-
-transformed_width, transformed_height = math.floor(width * ratio), math.floor(height * ratio)
-resized_image = image.resize((transformed_width, math.floor(transformed_height * 1.65)))
-
-print(f"Resized image size: {resized_image.width} x {resized_image.height}")
-
-resized_width, resized_height = resized_image.width, resized_image.height
-
-pixel_matrix = [[resized_image.getpixel((col, row)) for col in range(resized_width)] for row in range(resized_height)]
-
-print("Successfully constructed pixel matrix!")
 
 def weighted_brigtness_mapping(R: int, G: int, B: int) -> int:
     weigthed_r = 0.233 * R
@@ -38,10 +20,28 @@ def luminosity_brigtness_mapping(R: int, G: int, B: int) -> int:
     """
     return (0.2126 * R + 0.7152 * G + 0.0722 * B) // 3
 
+image = Image.open("test2.png")
+image = image.convert("RGB")
+width, height = image.width, image.height
+
+print("Successfully loaded image!")
+print(f"Image size: {width} x {height}")
+
+ratio = min(MAX_WIDTH / width, MAX_HEIGHT / height)
+
+transformed_width, transformed_height = math.floor(width * ratio), math.floor(height * ratio)
+resized_image = image.resize((transformed_width, math.floor(transformed_height * 1.65)))
+
+print(f"Resized image size: {resized_image.width} x {resized_image.height}")
+
+resized_width, resized_height = resized_image.width, resized_image.height
+
+pixel_matrix = [[resized_image.getpixel((col, row)) for col in range(resized_width)] for row in range(resized_height)]
+
+print("Successfully constructed pixel matrix!")
+
 brightness_matrix = [[luminosity_brigtness_mapping(pixel[0], pixel[1], pixel[2]) for pixel in row] for row in pixel_matrix]
 print("Successfully constructed brightness matrix!")
-
-ASCII_CHAR_SET = "`.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@" # 65 Chars
 
 """
 255 rgb value : 65 characters
@@ -55,6 +55,7 @@ for row in range(len(brightness_matrix)):
         char_index = min(max(math.floor(brightness_matrix[row][col] / 3.9), 0), len(ASCII_CHAR_SET) - 1)
         ascii_matrix[row][col] = ASCII_CHAR_SET[char_index] * 3 # stretch out the ascii art so that it's not squashed
 
+# print out in terminal
 for row in ascii_matrix:
     print("".join(row))
 
